b3e8959e333375a63d54e7725f28ee90fa6412f7
Vsevolod Burtyk <newemailaddress999999@gmail.com>
2025-03-26 13:20:40 +0200

    created second test and fixture

--- tests/test_filtering_lines_method.py
+++ tests/test_filtering_lines_method.py
@@ -1,11 +1,15 @@
 import pytest
 import sys
 import os
-
-# Додаємо шлях до директорії, де знаходиться main.py
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import main
+
+@pytest.fixture
+def sample_file(tmp_path):
+    file_path = tmp_path / "test_input.txt"
+    content = "Hello world\nPython is great\nKeyword here\nAnother line\n"
+    file_path.write_text(content, encoding="utf-8")
+    return file_path
 
 def test_file_not_found(capsys):
     main.filter_file_by_keyword("non_existent_file.txt", "keyword")
@@ -14,3 +18,12 @@ def test_file_not_found(capsys):
     captured = capsys.readouterr()
     assert "File 'non_existent_file.txt' not found" in captured.out
 
+
+def test_no_matching_lines(sample_file, tmp_path, capsys):
+    output_file = tmp_path / "filtered.txt"
+    main.filter_file_by_keyword(sample_file, "not_in_file")
+
+    assert not output_file.exists()
+
+    captured = capsys.readouterr()
+    assert "No matching lines found" in captured.out
